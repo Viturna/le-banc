e6d2a839ed6dd9bc212697cdd3d8ab83cae31e22
Viturna <148191882+Viturna@users.noreply.github.com>
2025-05-19 00:09:50 +0200

    correction profile

--- src/routes/profile/[username]/+page.svelte
+++ src/routes/profile/[username]/+page.svelte
@@ -1,27 +1,19 @@
+<!-- src/routes/profile/[username]/+page.svelte -->
 <script lang="ts">
-    import { getUserProfileByUsername } from '$lib/auth';
-	import { error } from '@sveltejs/kit';
-  
-    export let params: { username: string };
-    
-    let userProfile;
-  
-    // Récupérer le profil de l'utilisateur par son nom d'utilisateur (username)
-    try {
-      userProfile = await getUserProfileByUsername(params.username);
-    } catch (err) {
-      throw error(404, "User profile not found");
-    }
-  </script>
-  
-  {#if userProfile}
-    <h1>{userProfile.firstName} {userProfile.lastName}</h1>
-    <p>Email: {userProfile.email}</p>
-    <p>Username: {userProfile.username}</p>
-    <p>Quote: {userProfile.quote}</p>
-  
+	export let data: {
+		userProfile: {
+			firstName: string;
+			lastName: string;
+			email: string;
+			username: string;
+			quote?: string;
+		};
+	};
+</script>
 
-  {:else}
-    <p>Le profil demandé n'existe pas.</p>
-  {/if}
-  
+<h1>{data.userProfile.firstName} {data.userProfile.lastName}</h1>
+<p>Email: {data.userProfile.email}</p>
+<p>Username: {data.userProfile.username}</p>
+{#if data.userProfile.quote}
+	<p>Quote: {data.userProfile.quote}</p>
+{/if}
